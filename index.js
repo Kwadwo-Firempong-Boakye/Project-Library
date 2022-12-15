ce0016bd7d6f54484cb189e1e65f8e7db3c39229
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2022-12-15 16:16:54 +0000

    Add functions to add books to Library array

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+//Array of book objects
+
+let myLibrary = [];
+
+//Set up Add Book Popup
+
 const addBookButton = document.querySelector(".add-book-button");
 const popupContainer = document.querySelector(".pop-up");
 const glass = document.querySelector(".glass");
@@ -19,24 +25,53 @@ function hidePopup(e) {
 
 addBookButton.addEventListener("click", showPopup);
 glass.addEventListener("click", hidePopup);
+
+//Constructor function to create a book entry
+
+function Book (title, author, image, pages, readState) {
+    this.title = title,
+    this.author = author,
+    this.image = image,
+    this.pages = pages,
+    this.readState = readState,
+    this.info = function () {
+        return `${title} by ${author}, ${pages} pages, ${readState}`;
+    }
+};
     
+//Function to derive form inputs and create new book
+
+function addBookToLibrary(e) {
+
+    e.preventDefault();
+
+    const bookTitle = document.querySelector("#user-book-title").value;
+    const bookAuthor = document.querySelector("#user-book-author").value;
+    const bookImage = document.querySelector("#user-book-url").value;
+    const bookPages = document.querySelector("#user-book-pages").value;
+    const radioButtons = document.getElementsByName("read-state");
+    
+    let bookReadState;
+
+    radioButtons.forEach((item) => {
+        if (item.checked) {
+            bookReadState = item.value;
+        }
+    })
+
+    myLibrary.push(new Book(bookTitle, bookAuthor, bookImage, bookPages, bookReadState));
+
+}
+
+//Event Listener for popup submit
+
+popupContainer.addEventListener("submit", addBookToLibrary)
 
 
-// let myLibrary = [];
+//
 
-// function Book (title, author, pages, readState) {
-//     this.title = title,
-//     this.author = author,
-//     this.pages = pages,
-//     this.readState = readState,
-//     this.info = function () {
-//         return `${title} by ${author}, ${pages} pages, ${readState}`;
-//     }
-// };
+
 
 // const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "not read yet");
 // console.log(theHobbit.info());
 
-// function addBookToLibrary() {
-    
-// }
